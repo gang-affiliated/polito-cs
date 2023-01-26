7c418acbe039f96d67b4fae15a6e7753630f9c68
dou <119308537+gang-affiliated@users.noreply.github.com>
2023-01-26 17:15:36 +0100

    Add files via upload

---
+++ lab7-1.6&1.7.py
@@ -0,0 +1,17 @@
+import random
+llist = []
+for i in range(5):
+    llist.append(random.randint(0, 99))
+
+print(llist)
+
+llist.sort()
+
+print(llist)
+
+def sum_without_smallest(v):
+    v.remove(min(v))
+    sonuc = sum(v)
+    return sonuc
+
+print(f"here is the sum w/o the smallest value: {sum_without_smallest(llist)}")
